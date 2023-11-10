483e32d994b7fbc0390323bd260173544d09e263
Alejandro Ochoa <115182554+ToastedPickle2@users.noreply.github.com>
2023-11-10 12:55:48 -0800

    Update mapty.js

--- mapty.js
+++ mapty.js
@@ -11,12 +11,25 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+const noWorkoutDisplay = document.querySelector(".no-workouts-display");
+//prettier-ignore
+const leafletPopup = document.querySelectorAll(".leaflet-popup-content-wrapper");
+//prettier-ignore
+const noWorkoutDisplayText = document.querySelector(".no-workouts-display-text");
+const clearBtn = document.querySelector(".clear-btn");
+const clearBtnWrapper = document.querySelector(".clear-btn-wrapper");
+const sortBtn = document.querySelector(".sort-btn");
+const alertContainer = document.querySelector(".alert-container");
+const checkContainer = document.querySelector(
+  ".workout-confirmation-message-container"
+);
 
 class App {
   #map;
   #mapZoomLevel = 13;
   #mapEvent;
   #workouts = [];
+  #workoutsArray = [];
 
   constructor() {
     // get user's position
@@ -32,7 +45,17 @@ class App {
 
     containerWorkouts.addEventListener("click", this._moveToPopup.bind(this));
 
+    sortBtn.addEventListener("click", this._sort.bind(this));
+
+    // leafletPopup.forEach((el) => el.on("click", this._moveToPopup.bind(this)));
+
     this._close();
+
+    this._clearAll();
+  }
+
+  _sortWorkouts() {
+    this.#workoutsArray.sort((a, b) => a.distance - b.distance); // Sort by distance (you can change the sorting criteria)
   }
 
   _getPosition() {
@@ -77,6 +100,11 @@ class App {
     console.log(mapE);
     form.classList.remove("hidden");
     inputDistance.focus();
+
+    noWorkoutDisplay.classList.add("hidden");
+    noWorkoutDisplayText.classList.add("hide-text");
+
+    // clearBtnWrapper.style.display = "flex";
   }
 
   _hideForm() {
@@ -119,8 +147,13 @@ class App {
       if (
         !validInputs(distance, duration, cadence) ||
         !allPositive(distance, duration, cadence)
-      )
-        return alert("Inputs have to be positive numbers!");
+      ) {
+        alertContainer.style.opacity = "100%";
+        return;
+      } else {
+        alertContainer.style.opacity = "0%";
+      }
+      // return alert("Inputs have to be positive numbers!");
 
       workout = new Running([lat, lng], distance, duration, cadence);
     }
@@ -132,8 +165,15 @@ class App {
       if (
         !validInputs(distance, duration, elevation) ||
         !allPositive(distance, duration)
-      )
-        return alert("Inputs have to be positive numbers!");
+      ) {
+        // alertContainer.style.display = "flex";
+        alertContainer.style.opacity = "100%";
+        return;
+      } else {
+        // alertContainer.style.display = "none";
+        alertContainer.style.opacity = "0%";
+      }
+      // return alert("Inputs have to be positive numbers!");
 
       workout = new Cycling([lat, lng], distance, duration, elevation);
     }
@@ -147,6 +187,8 @@ class App {
     // Render workout on list
     this._renderWorkout(workout);
 
+    // this._sort();
+
     // Hide form and clear input fields
     this._hideForm();
 
@@ -156,6 +198,18 @@ class App {
     this._close(workout);
   }
 
+  _clearAll() {
+    if (containerWorkouts.querySelectorAll("li").length <= 0) {
+    } else if (containerWorkouts.querySelectorAll("li").length > 0) {
+    }
+    clearBtn.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      localStorage.removeItem("workouts");
+      window.location.reload();
+    });
+  }
+
   _close() {
     const self = this;
 
@@ -163,15 +217,28 @@ class App {
       el.addEventListener("click", function (e) {
         e.preventDefault();
 
-        console.log("#WORKouts", self.#workouts);
-
-        const workoutEl = el.closest(".workout");
+        const workoutEl = e.target.closest(".workout");
+        workoutEl.classList.remove("border");
 
         if (workoutEl) {
-          workoutEl.classList.add("form__row--hidden");
-          // prettier-ignore
-          const workout = self.#workouts.find((work) => work.id === workoutEl.dataset.id);
-          console.log(workout);
+          workoutEl.classList.add("hidden");
+          setTimeout(
+            () => {
+              workoutEl.classList.add("form__row--hidden");
+              const workoutId = workoutEl.dataset.id;
+
+              self.#workouts = self.#workouts.filter(
+                (workout) => workout.id !== workoutId
+              );
+
+              localStorage.setItem("workouts", JSON.stringify(self.#workouts));
+              noWorkoutDisplayText.classList.add("hide-text");
+
+              window.location.reload(); // reloads the page
+            },
+
+            1000
+          );
         }
 
         if (!workoutEl) return;
@@ -180,7 +247,7 @@ class App {
   }
 
   _renderWorkoutMarker(workout) {
-    console.log("WORKOUT 1", workout);
+    // console.log("WORKOUT 1", workout);
 
     L.marker(workout.coords)
       .addTo(this.#map)
@@ -204,7 +271,14 @@ class App {
   }
 
   _renderWorkout(workout) {
-    console.log("WORKOUT 2", workout);
+    this.#workoutsArray.push(workout);
+    console.log("workouts array", this.#workoutsArray);
+
+    if (this.#workoutsArray.length > 1) {
+      this.#workoutsArray.sort((a, b) => a.distance - b.distance);
+      console.log("sorted workouts array", this.#workoutsArray);
+    }
+
     let html = `
         <li class="workout workout--${workout.type}" data-id="${workout.id}">
             <button class="description__close--btn">x</button>
@@ -227,7 +301,9 @@ class App {
       html += `
             <div class="workout__details">
               <span class="workout__icon">⚡️</span>
-              <span class="workout__value">${workout.pace.toFixed(1)}</span>
+              <span class="workout__value">${
+                workout.pace ? workout.pace.toFixed(1) : ""
+              }</span>
               <span class="workout__unit">min/km</span>
             </div>
             <div class="workout__details">
@@ -241,7 +317,9 @@ class App {
       html += `
             <div class="workout__details">
               <span class="workout__icon">⚡️</span>
-              <span class="workout__value">${workout.speed.toFixed(1)}</span>
+              <span class="workout__value">${
+                workout.speed ? workout.speed.toFixed(1) : ""
+              }</span>
               <span class="workout__unit">km/h</span>
             </div>
             <div class="workout__details">
@@ -252,6 +330,24 @@ class App {
           </li> -->`;
 
     form.insertAdjacentHTML("afterend", html);
+
+    checkContainer.style.opacity = "100%";
+    setTimeout(() => (checkContainer.style.opacity = "0%"), 2000);
+
+    clearBtnWrapper.style.display = "flex";
+  }
+
+  _sort() {
+    console.log("click");
+    console.log("Sort Method", this.#workoutsArray);
+
+    this._sortWorkouts();
+
+    // const workoutsContainer = document.querySelector(".div-workouts");
+    // workoutsContainer.innerHTML = "";
+    // containerWorkouts.innerHTML = "";
+
+    this.#workoutsArray.forEach((workout) => this._renderWorkout(workout));
   }
 
   _moveToPopup(e) {
@@ -261,6 +357,7 @@ class App {
     // prettier-ignore
     const workout = this.#workouts.find((work) => work.id === workoutEl.dataset.id);
     // the setView method is a leaflet method
+
     this.#map.setView(workout.coords, this.#mapZoomLevel, {
       animate: true,
       pan: {
@@ -361,7 +458,33 @@ class Cycling extends Workout {
   }
 }
 
-// const run1 = new Running([39, -12], 24, 178);
-// const cycling1 = new Cycling([39, -12], 27, 95, 523);
-// console.log(run1);
-// console.log(cycling1);
+if (containerWorkouts.querySelectorAll("li").length > 0) {
+  noWorkoutDisplay.classList.add("hidden");
+  noWorkoutDisplayText.classList.add("hide-text");
+}
+
+leafletPopup.forEach((el) =>
+  el.addEventListener("click", function () {
+    console.log("SUCCESS");
+    leafletPopup.style.fontSize = "20rem";
+  })
+);
+
+const workoutBox = document.querySelectorAll(".workout");
+let selectedWorkout = null;
+
+workoutBox.forEach((el) =>
+  el.addEventListener("click", function (e) {
+    const workoutEl = e.target.closest(".workout");
+
+    if (workoutEl) {
+      if (selectedWorkout) {
+        selectedWorkout.classList.remove("border");
+      }
+
+      workoutEl.classList.add("border");
+
+      selectedWorkout = workoutEl;
+    }
+  })
+);
